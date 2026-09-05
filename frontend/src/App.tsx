395b895393c9fee4c30280852641978deb793280
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-05 21:26:26 +1000

    feat: init vite with react-ts as the template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+function App() {
+
+  return (
+    <main className='flex items-center justify-center bg-red-500'>
+      Hello
+    </main>
+  )
+}
+
+export default App
