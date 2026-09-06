89dd57b93bb0d0f702971761fb79f103e7030e6e
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 16:55:59 +1000

    feat(IdenSer): add SeedData to insert admin to Users table

---
+++ services/IdentityService/Data/SeedData.cs
@@ -0,0 +1,30 @@
+using IdentityService.Models;
+
+namespace IdentityService.Data;
+
+public static class SeedData
+{
+    public static void Init(IServiceProvider serviceProvider)
+    {
+        var context = serviceProvider.GetRequiredService<IdentityServiceContext>();
+
+        if (context.Users.Any()) {
+            return; // DB has already been seeded.
+        }
+
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword("Admin12!");
+
+        var admin = new User
+        {
+            FirstName = "Admin",
+            LastName = "User",
+            Username = "admin",
+            Email = "admin@email.com",
+            PasswordHash = hashedPassword,
+            Role = Role.ADMIN,
+        };
+
+        context.Add(admin);
+        context.SaveChanges();
+    }
+}
