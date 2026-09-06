d7d95cb5b1ed9c572269eab80f2289fac4fd3f39
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 16:54:27 +1000

    feat(IdenSer): add User class for table

---
+++ services/IdentityService/Models/User.cs
@@ -0,0 +1,42 @@
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
+
+[Index(nameof(Username), IsUnique = true)]
+[Index(nameof(Email), IsUnique = true)]
+public class User
+{
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string LastName { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string Username { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; }
+
+    [Required]
+    public Role Role { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
