0e42052eb5755dbec7f61764eda52b5c4b0fcdb8
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-06 15:03:22 +1000

    feat(frontend): add footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex items-center justify-center p-4 bg-gray-50 border-t border-gray-200">
+            <p>William Hok</p>
+        </footer>
+    )
+}
