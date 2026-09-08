3fb91cd8ceb955bd92426efa1b530916aa654eeb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-08 12:55:09 +1000

    feat(frontend): add user and token validation after refresh using lcoalstroage

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,7 +1,8 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import type { CreateUserInput, User } from "../types/User";
 import { authApi } from "../api/auth";
 import { useNavigate } from "react-router-dom";
+import type { AuthResponse } from "../types/Auth";
 
 interface AuthContextType {
     loggedInUser: User | null
@@ -16,17 +17,37 @@ export function AuthContextProvider({ children }: { children: React.ReactNode })
     const [loggedInUser, setLoggedInUser] = useState<User | null>(null)
     const navigate = useNavigate()
 
+    useEffect(() => {
+        if (loggedInUser == null) {
+            navigate("/")
+        }
+        
+        const user = localStorage.getItem("user")
+        const token = localStorage.getItem("token")
+
+        if (user && token) {
+            setLoggedInUser(JSON.parse(user))
+        }
+    })
+
     async function signUp(input: CreateUserInput): Promise<void> {
         await authApi.register(input)
     }
 
     async function signIn(email: string, password: string): Promise<User> {
-        const user =  await authApi.login(email, password)
+        const { user, token }: AuthResponse =  await authApi.login(email, password)
+
+        localStorage.setItem("user", JSON.stringify(user))
+        localStorage.setItem("token", token)
+
         setLoggedInUser(user)
         return user
     }
 
     function signOut(): void {
+        localStorage.removeItem("user")
+        localStorage.removeItem("token")
+
         setLoggedInUser(null)
         navigate("/")
     }
