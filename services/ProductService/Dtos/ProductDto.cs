fd7bf47849e3598087df85004f4343ba3c6335cb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-15 11:24:13 +1000

    feat(ProdSer): add ProductDto to response the frontend

---
+++ services/ProductService/Dtos/ProductDto.cs
@@ -0,0 +1,9 @@
+public class ProductDto
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
