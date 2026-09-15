582a021838fe113b94d6da576c817bfb70f35afb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-15 16:32:13 +1000

    fix(frontend): remove old axios file and use decicated api for each service

--- frontend/src/api/axios.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: import.meta.env.IDENTITY_SERVICE_PORT || "http://localhost:5000/api"
-})
--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -1,9 +1,13 @@
+import axios from "axios";
 import type { User } from "../types/User";
-import axios from "./axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_IDENTITY_SERVICE_PORT || "http://localhost:5000/api"
+})
 
 export const usersApi = {
   getAllUsers: async (): Promise<User[]> => {
-    const response = await axios.get<User[]>("/users");
+    const response = await api.get<User[]>("/users");
     return response.data;
   },
 };
