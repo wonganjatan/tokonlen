4c37f381a480dac9becff834ef48f2768af90dc6
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-09-23 14:36:43 +1000

    feat(frontend): add api call to get all cart items

--- frontend/src/api/carts.ts
+++ frontend/src/api/carts.ts
@@ -5,6 +5,11 @@ const baseURL = import.meta.env.VITE_CART_SERVICE_PORT || "http://localhost:5002
 const api = authMiddleware(baseURL)
 
 export const cartsApi = {
+    getAll: async (): Promise<CartItemResponse[]> => {
+        const res = await api.get<CartItemResponse[]>("/carts")
+        return res.data
+    },
+
     create: async (request: CartItemRequest): Promise<CartItemResponse> => {
         const res = await api.post<CartItemResponse>("/carts", request)
         return res.data
